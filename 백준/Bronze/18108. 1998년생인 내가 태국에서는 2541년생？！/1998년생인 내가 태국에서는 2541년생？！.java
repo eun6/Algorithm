7a5a043cf7fe7430d804335d09384864ba6b5c57
ye eun <86345507+eun6@users.noreply.github.com>
2023-09-23 03:26:18 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 208 ms, Memory: 17688 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String args[]) {
+        //서기 543년이 시작.
+        Scanner scanner = new Scanner(System.in);
+
+       int y = scanner.nextInt();
+       System.out.println(y-543);
+       scanner.close();
+    }
+}
