a8a5bf5013be376bf593bbe75fa871c753a06b0c
ye eun <86345507+eun6@users.noreply.github.com>
2023-10-31 23:08:19 +0900

    [Bronze III] Title: 네 수, Time: 128 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Bronze/10824. 네 수/네 수.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String a = st.nextToken();
+        String b = st.nextToken();
+        String c = st.nextToken();
+        String d = st.nextToken();
+
+        br.close();
+        long first = Long.parseLong(a+b);
+        long second = Long.parseLong(c+d);
+        System.out.println(first+second);
+    }
+}
