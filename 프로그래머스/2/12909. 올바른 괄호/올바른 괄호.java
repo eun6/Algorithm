5a43d074dc71898ce527c9de7b599de68846d29c
ye eun <86345507+eun6@users.noreply.github.com>
2023-11-22 09:29:00 +0900

    [level 2] Title: 올바른 괄호, Time: 17.93 ms, Memory: 53.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        Stack<Character> stack = new Stack<>();
+        for(int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
+            if(c == '(') {
+                stack.push(c);
+            } else if (c == ')') {
+                if(stack.isEmpty()) stack.push(c);
+                else stack.pop();
+            }
+        }
+        if(stack.isEmpty()) answer = true;
+        else answer = false;
+        return answer;
+    }
+}
